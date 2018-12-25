c265e1c134d8171f95c8f8313fe7fa26a693d3a6
The-hiding-spot <kevin.mould1@gmail.com>
2018-12-24 20:33:20 -0500

    Update Linux Administration Tool.sh

--- Linux Administration Tool.sh
+++ Linux Administration Tool.sh
@@ -1,51 +1,91 @@
 #!/bin/bash
 
+clear
+
 while true ; do
-echo "1. Add a User to the System."
-echo "2. Remove a User from the System."
-echo "3. Change a User's password."
-echo "4. Show active network connections."
-echo "5. Show failed login attempts."
-echo "0. Logout. (Exit)"
+
+echo "1. Add a user to the system. (Sudo required)"
+echo "2. Remove a user from the system. (Sudo required)"
+echo "3. Change a user's password.(Sudo required)"
+echo "4. List of currently logged in users."
+echo "5. Display disk usage statistics."
+echo "6. Show active network connections."
+echo "7. Show failed login attempts. (Sudo required)"
+echo "8. Display the current date and time."
+echo "9. Display the system hostname."
+echo "10. Print the current working directory."
 echo
 
-echo -n "Select an option to continue, or 0 to exit: "
-read option
+
+echo -n "Select an option between 1-10 to continue, or 'exit' to exit the script: "
+read OPTION
+clear
 echo
 
-case $option in
-1)
-echo "Type the user's username to add them to the system."
-read username
-sudo useradd $username
-echo "User has been created."
-;;
-2)
-echo "Type the user's username to remove them from the system."
-read username
-sudo userdel $username
-echo "User has been removed from the system."
-;;
-3)
-echo "Changing password. Please specify the user's account"
-read username
-sudo passwd $username
-echo "password changed."
-;;
-4)
-echo "Displaying active network connections"
-sudo netstat -a | more
-;;
-5)
-echo "Displaying failed login attempts"
-sudo lastb
-;;
-0)
-echo "Logging off..."
-exit
-;;
-*)
-echo "Incorect value, please type a number between 0 and 5."
-;;
+
+case $OPTION in
+
+    1)
+        echo "Type the user's username to add them to the system."
+        read USERNAME
+        sudo useradd $USERNAME
+        echo "User has been created."
+        ;;
+    2)
+        echo "Type the user's username to remove them from the system."
+        read USERNAME
+        sudo userdel $USERNAME
+        echo "User has been removed from the system."
+        ;;
+    3)
+        echo "Changing password. Please specify the user's account"
+        read USERNAME
+        sudo passwd $USERNAME
+        echo "password changed."
+        ;;
+    4)
+        echo "These users are currently logged in: "
+        who | awk '{print $1}' | sort -u | more
+        ;;
+    5)
+        echo "Disk usage statistics: "
+        df -h | awk '{print $4, $5, $6}' | more
+        ;;
+    6)
+        echo "Displaying active network connections"
+        netstat -atu | more
+        ;;
+    7)
+        echo "Displaying failed login attempts"
+        sudo lastb | tac | more
+        ;;
+    8)
+        echo "The current date and time is: "
+        date
+        ;;
+    9)
+        echo "The system hostname is: "
+        hostname
+        ;;
+    10)
+        echo "The current working directory is: "
+        pwd
+        ;;
+    exit | EXIT)
+        echo "Exited Script."
+        exit
+        ;;
+    *)
+        echo "Incorect value, please type a number between 1 and 10, or 'exit' to exit the script."
+        ;;
+
 esac
+
+
+echo
+echo "End of command, press any button to return to the menu."
+read OPTION
+clear
+
+
 done
