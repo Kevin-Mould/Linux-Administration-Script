a2ee4593c27170ba538d7887c6eeb485e4c11eda
kmould <kmould@localhost.localdomain>
2019-12-28 01:26:52 -0500

    Added find a file option and changed option 10 to a nmap port scan.

--- Linux Administration Tool.sh
+++ Linux Administration Tool.sh
@@ -5,16 +5,16 @@ clear
 while true ; do
 
 echo "0. Exit script"
-echo "1. Add a user to the system. (Sudo required)"
-echo "2. Remove a user from the system. (Sudo required)"
-echo "3. Change a user's password.(Sudo required)"
-echo "4. List of currently logged in users."
-echo "5. Display disk usage statistics."
-echo "6. Show active network connections."
-echo "7. Show failed login attempts. (Sudo required)"
-echo "8. Display the current date and time."
+echo "1. Add a user to the system."
+echo "2. Remove a user from the system."
+echo "3. Change a user's password."
+echo "4. Find a file on the system."
+echo "5. List of currently logged in users."
+echo "6. Display disk usage statistics."
+echo "7. Show active network connections."
+echo "8. Show failed login attempts."
 echo "9. Display the system hostname."
-echo "10. Print the current working directory."
+echo "10. Perform a port scan of this computer using nmap."
 echo
 
 
@@ -23,6 +23,23 @@ read OPTION
 clear
 echo
 
+checkNmapPackage() {
+	echo "Checking for required package Nmap..."
+	if rpm -qa | grep {nmap}; then
+		echo "Nmap is installed."
+	else
+		echo "Nmap is not installed. Install it y/n?"
+		read "TEST"
+		if [ "$TEST" = "y" ]; then
+			sudo yum install nmap
+		else
+			return
+		fi
+	fi
+	echo "I'm using your IP address to perform a port scan using nmap..."
+	MYIP=$(ip -o addr show up primary scope global | while read -r num dev fam addr rest; do echo ${addr%/*}; done)
+	nmap -Pn -T4 -A -p- $MYIP
+}		
 
 case $OPTION in
 
@@ -35,7 +52,7 @@ case $OPTION in
         read USERNAME
 
 	: '
-	grep statement evulates to boolean True if the inputed USERNAME does not 	exist.
+	grep statement evulates to boolean True if the inputed USERNAME does not exist.
 	grep statement evulates to boolean False if the inputed USERNAME exists.
 	'
 
@@ -43,7 +60,7 @@ case $OPTION in
         	echo "$USERNAME has already been created. No action taken."
 	else
 		sudo useradd $USERNAME
-        	echo "User has been created."
+        	echo "$USERNAME has been created."
 	fi
         ;;
     2)
@@ -69,33 +86,33 @@ case $OPTION in
         fi
 	;;
     4)
+	echo "Type the filename to search the system."
+	read FILENAME
+	sudo find / -name "$FILENAME" 
+	;;
+    5)
         echo "These users are currently logged in: "
         who | awk '{print $1}' | less
         ;;
-    5)
+    6)
         echo "Disk usage statistics: "
         df -h | awk '{print $4, $5, $6}' | less
         ;;
-    6)
+    7)
         echo "Displaying active network connections"
         netstat -atu | less
         ;;
-    7)
+    8)
         echo "Displaying failed login attempts"
         sudo lastb | less
-        ;;
-    8)
-        echo "The current date and time is: "
-        date
         ;;
     9)
         echo "The system hostname is: "
         hostname
         ;;
-    10)
-        echo "The current working directory is: "
-        pwd
-        ;;
+   10)
+	checkNmapPackage
+	;;
     *)
         echo "Incorect value, please type a number between 1 and 10, or 'exit' to exit the script."
         ;;
